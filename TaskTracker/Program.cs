9eca460d2fc3ab65d5b939661974f5345d8face8
Melissa Hegney <mhegney@stmartin.edu>
2026-01-27 17:22:21 -0800

    add do while loop so program will reprompt

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -1,15 +1,25 @@
+var shouldContinue = true;
+do {
+    Console.WriteLine("Welcome to TaskTracker!");
+    Console.WriteLine("1. List Tasks");
+    Console.WriteLine("2. About");
+    Console.WriteLine("3. Exit");
+    Console.Write("Choose an option: ");
 
-Console.WriteLine("Welcome to TaskTracker!");
-Console.WriteLine("1. List Tasks");
-Console.WriteLine("2. About");
-Console.Write("Choose an option: ");
+    int.TryParse(Console.ReadLine(), null, out int input);
 
-var input = Console.ReadLine();
+    if (input == 2)
+    {
+        Console.WriteLine("Melissa Hegney");
+        Console.WriteLine("Merlissa09");
+        Console.WriteLine(DateTime.IsLeapYear(1998));
+        Console.WriteLine(DateTime.Now);
+    }
 
-if (input == "2")
-{
-    Console.WriteLine("Melissa Hegney");
-    Console.WriteLine("Merlissa09");
-    Console.WriteLine(DateTime.IsLeapYear(1998));
-    Console.WriteLine(DateTime.Now);
-}
+    if(input == 3)
+    {
+        shouldContinue = false;
+    }
+
+    // make it so that the program does't close after an action is done
+} while (shouldContinue);
