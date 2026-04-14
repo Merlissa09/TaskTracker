05c7beb1fecb6650c2a5d8e150d6c284d284adc2
jagrant59328 <jagrant59328@gmail.com>
2026-04-14 16:54:25 -0700

    Recurring-Taskitem

---
+++ TaskDomain/recurringtaskitems.cs
@@ -0,0 +1,33 @@
+
+using TaskDomain;
+//This class represents a recurring task so different from the actual interface
+public class RecurringTask : ITaskable
+{
+    private bool Iscomplete { get; set; }
+    private string? Description { get; set; }
+    private string? GetFrequency { get; }
+    public string GetTitle()
+    {
+        return "Title";
+    }
+
+    public string GetDescription()
+    {
+        if (Description is string)
+        {
+            return Description;
+        }
+        return string.Empty;
+    }
+
+    public bool IsComplete()
+    {
+        return Iscomplete;
+    }
+
+    public bool MarkComplete()
+    {
+        Iscomplete = true;
+        return Iscomplete;
+    }
+}
