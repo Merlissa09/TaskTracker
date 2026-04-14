a4b953f3bae066035eed2398198294bb56b12652
ilovetweety <donald.y.choe@gmail.com>
2026-04-14 16:57:55 -0700

    to add DeadlineTaske

---
+++ TaskDomain/DeadlineTask.cs
@@ -0,0 +1,57 @@
+namespace TaskDomain;
+
+public class DeadlineTask : ITaskable
+{
+    private string _title;
+    private string _description;
+    private bool _complete;
+    private DateTime _dueDate;
+
+    public DeadlineTask(string title, string description, DateTime dueDate)
+    {
+        _title = title;
+        _description = description;
+        _dueDate = dueDate;
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public bool IsComplete()
+    {
+        return _complete;
+    }
+
+    public bool MarkComplete()
+    {
+        if (_complete)
+        {
+            return false;
+        }
+
+        _complete = true;
+        return true;
+    }
+
+    public DateTime GetDueDate()
+    {
+        return _dueDate;
+    }
+
+    public void PrintDeadlineTask()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Description: {_description}");
+        Console.WriteLine($"Due Date: {_dueDate}");
+        Console.WriteLine($"Is Complete: {_complete}");
+        
+    }
+    
+}
